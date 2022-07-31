d2618bb5b113e567d52ba1515e8f002ec61bf55d
Damebeus <99093997+Damebeus@users.noreply.github.com>
2022-07-31 03:29:07 -0300

    welcome

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Welcome from "../Welcome/Welcome";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <Welcome />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
