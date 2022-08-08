72fc841d5a54e5b74396d369f14e9bb8c4e18e7c
Damebeus <99093997+Damebeus@users.noreply.github.com>
2022-08-07 22:35:53 -0300

    form activo

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,11 +10,76 @@ import { AiFillLinkedin } from "react-icons/ai";
 import { SiGooglemaps } from "react-icons/si";
 import "aos/dist/aos.css";
 import Aos from "aos";
-
+import { useForm, ValidationError } from "@formspree/react";
 const Contact = () => {
   useEffect(() => {
     Aos.init({ duration: 2000, once: false });
   }, []);
+  const [state, handleSubmit] = useForm("xqkjzvoq");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
+  return (
+    <section className='section' id='Contact'>
+      <div className={style.container}>
+        <div className={style.title}>
+          <h1>Contact </h1>
+        </div>
+
+        <div className={style.columna}>
+          <div className={style.columnaForm}>
+            <div className={style.formInput}>
+              <form onSubmit={handleSubmit}>
+                <div className={style.row} data-aos='fade-right'>
+                  <div className={style.formGroup}>
+                    <input type='text' id='name' required />
+                    <label htmlFor='name'>
+                      <AiOutlineUser />
+                       Name
+                    </label>
+                  </div>
+                  <div className={style.formGroup}>
+                    <input type='text' id='number' required />
+                    <label htmlFor='text'>
+                      <AiOutlinePhone />
+                       Number
+                    </label>
+                  </div>
+                </div>
+                <div className={style.formGroup} data-aos='fade-left'>
+                  <input type='email' id='email' name='email' required />
+                  <label htmlFor='email'>
+                    <ValidationError
+                      prefix='Email'
+                      field='email'
+                      errors={state.errors}
+                    />
+                    <AiOutlineMail />  Email
+                  </label>
+                </div>
+                <div className={style.formGroup} data-aos='fade-right'>
+                  <textarea id='message' rows={8} required></textarea>
+                  <label htmlFor='message'>
+                    <BiMessageDetail />
+                     Message
+                  </label>
+                </div>
+                <div className={style.btn} data-aos='fade-up'>
+                  <button
+                    className={style.slide}
+                    type='submit'
+                    disabled={state.submitting}
+                  >
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
